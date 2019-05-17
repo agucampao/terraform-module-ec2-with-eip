121e7ba484ef31c85fba0b378db79943f84a0d04
Your Name <you@example.com>
2019-05-17 14:14:57 -0300

    adding some features

--- sg.tf
+++ sg.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "sg" {
   name_prefix        = "${var.project_name}-${var.environment}-"
-  description = "Security-Group-For ${var.project_name}-${var.environment}"
+  description = "Security Group ${var.project_name}-${var.environment}"
   vpc_id      = "${var.vpc_id}"
 
   ingress {
